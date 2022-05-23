7b0482fecca8c4fa0b37bb67afc2d093529ca493
mcaox <84007860+mcaox@users.noreply.github.com>
2022-05-23 15:40:05 +0200

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+mkdir -p ~/.streamlit/
+mkdir -p ~/.streamlit/
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+[theme]\n\
+base = 'light'\n\
+\n\
+" > ~/.streamlit/config.toml
